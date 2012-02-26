a716318af1090a349cdcbf437a8c32b84c30475f
magnusmanske <magnusmanske@googlemail.com>
2012-02-26 20:06:36 +0000

    mobile fixes

---
+++ config.js
@@ -0,0 +1,15 @@
+var gentle_config = {
+
+	default_plugins : [
+		'plugins/synbiota.js' ,
+		'plugins/find_in_sequence.js' ,
+		'plugins/prepend_append_sequence.js' ,
+		'plugins/export_as_file.js'
+	] ,
+	
+	deactivated_plugins : [
+	] ,
+	
+	proxy : './proxy.php'
+	
+} ;
