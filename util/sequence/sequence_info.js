32e5e4f34e417355dc25cf000b30f2066521e3c2
Magnus Manske <magnusmanske@googlemail.com>
2012-03-22 23:48:35 +0000

    starting on annotation editing

--- util/sequence/sequence_info.js
+++ util/sequence/sequence_info.js
@@ -1,16 +1,20 @@
 //________________________________________________________________________________________
 // SequenceInfoDialog base class
 function SequenceInfoDialog ( sc ) {
-	console.log ( "Don't do that" ) ;
 }
 
 SequenceInfoDialog.prototype.onCancelButton = function () {}
 SequenceInfoDialog.prototype.onSaveButton = function () {}
 
+SequenceInfoDialog.prototype.closeDialog = function () {
+	$('#'+gentle.sequence_info_dialog.sid).modal('hide');
+	$('#'+gentle.sequence_info_dialog.sid).remove();
+}
+
 
 //________________________________________________________________________________________
 // SID DNA
-SequenceInfoDialogDNA.prototype = new SequenceCanvas() ;
+SequenceInfoDialogDNA.prototype = new SequenceInfoDialog() ;
 SequenceInfoDialogDNA.prototype.constructor = SequenceInfoDialogDNA ;
 
 function SequenceInfoDialogDNA ( sc ) {
@@ -26,7 +30,7 @@ function SequenceInfoDialogDNA ( sc ) {
 	
 	$('#'+this.sid).remove();
 	var dialogContainer = $("<div/>");
-	dialogContainer.load ( "public/templates/sequence_info_dialog_dna.html", function(){
+	dialogContainer.load ( "public/templates/sequence_info_dialog_dna.html", function() {
 		var me = gentle.sequence_info_dialog ;
 		
 		dialogContainer.appendTo("#all");
@@ -36,17 +40,53 @@ function SequenceInfoDialogDNA ( sc ) {
 		$('#sid_name').val ( me.data.name ) ;
 		$('#sid_desc').val ( me.data.desc ) ;
 		
+		var h = '' ;
+		$.each ( sc.sequence.features , function ( k , v ) {
+			var type = v['_type'] || 'unknown' ;
+			if ( type == 'source' ) return ;
+			
+			var from = v['_range'][0].from ;
+			var to = v['_range'][v['_range'].length-1].to ;
+			var rc = v['_range'][0].rc ? "&larr;" : "&rarr;" ;
+
+			// The name and desc finding is code duplication, and should be factorized!
+			var name = '' ;
+			if ( v['gene'] !== undefined ) name = v['gene'] ;
+			else if ( v['product'] !== undefined ) name = v['product'] ;
+			else if ( v['name'] !== undefined ) name = v['name'] ;
+			name = name.replace(/^"/,'').replace(/"$/,'') ;
+
+			h += "<tr rowid='" + k + "'>" ;
+			h += "<td>" + name + "</td>" ;
+			h += "<td>" + type + "</td>" ;
+			h += "<td>" + from + " &ndash; " + to + " " + rc + "</td>" ;
+			h += "</tr>" ;
+		} ) ;
+		$('#sid_annotation_tbody').html ( h ) ;
+		$('#sid_annotation_tbody tr').click ( function ( o ) {
+			gentle.sequence_info_dialog.showAnnotationDetails ( $(o.currentTarget).attr('rowid') ) ;
+		} ) ;
+		
 		$('#sid_tab').tab('show') ;
 		$('#sid_tab a:first').tab('show')
 	} ) ;
 }
 
+SequenceInfoDialogDNA.prototype.showAnnotationDetails = function ( key ) {
+	$("#sid_annotation_details").html ( "TODO! " + key ) ;
+} ;
+
 SequenceInfoDialogDNA.prototype.onCancelButton = function () {
+	gentle.sequence_info_dialog.closeDialog() ;
 	$('#'+gentle.sequence_info_dialog.sid).modal('hide');
 	$('#'+gentle.sequence_info_dialog.sid).remove();
 }
 
 SequenceInfoDialogDNA.prototype.onSaveButton = function () {
-	$('#'+gentle.sequence_info_dialog.sid).modal('hide');
-	$('#'+gentle.sequence_info_dialog.sid).remove();
+	var me = gentle.sequence_info_dialog ;
+	
+	me.sc.sequence.name = $('#sid_name').val() ;
+	me.sc.sequence.desc = $('#sid_desc').val() ;
+	
+	me.closeDialog() ;
 }
