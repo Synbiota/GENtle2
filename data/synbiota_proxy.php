b2cf53b124f1a38100091e48d8a83a68c432f2b2
Magnus Manske <magnusmanske@googlemail.com>
2012-04-15 22:08:32 +0100

    turning ssl certificate checking off

--- data/synbiota_proxy.php
+++ data/synbiota_proxy.php
@@ -12,6 +12,7 @@ function do_post_curl ( $url , $data ) {
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_POST, true);
 	
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
