fc540034cb7d5eb37f387b1b49a163eb46df85fa
Magnus Manske <magnusmanske@googlemail.com>
2012-04-22 17:13:58 +0100

    synbiota parts loading

--- plugins/synbiota.js
+++ plugins/synbiota.js
@@ -40,7 +40,92 @@ synbiota.prototype.saveToSynbiota = function () {
 }
 
 synbiota.prototype.findParts = function () {
-	alert ( "!" ) ;
+	var sc = this.getCurrentSequenceCanvas() ;
+	if ( sc === undefined ) return ; // Paranoia
+
+	var me = this ;
+	me.fpdid = 'synbiota_find_parts_dialog' ;
+	me.fpd_results = [] ;
+	
+	var h = '' ;
+	h += '<div class="modal" id="' + this.fpdid + '">' ;
+	h += '<div class="modal-header"><a class="close" data-dismiss="modal"></a><h3>Find parts</h3></div>' ;
+	h += '<div class="modal-body">' ;
+
+	h += "<div><form id='fpd_form'><input type='text' id='fpd_query' value='cloning' size='50' /> <input type='submit' value='Find parts' /></form></div>" ;
+	
+	h += "<div id='fpd_results' class='well' style='min-height:300px;max-height:300px;width:95%;overflow:auto;'>" ;
+	h += "</div>" ; // fpd_results
+
+	h += '</div>' ; // modal-body
+
+//	h += '<div class="modal-footer"><a href="#" class="btn btn-primary" onclick=\'gentle.sequence_info_dialog.onSaveButton();return false\'>Save changes</a>' ;
+//	h += '<a href="#" class="btn" onclick=\'gentle.sequence_info_dialog.onCancelButton();return false\'>Cancel</a></div>' ;
+	h += '</div>' ; // modal
+
+//</div>
+
+	$('#'+this.fpdid).remove() ;
+	$('body').append ( h ) ;
+	$('#'+this.fpdid).modal();
+	$('#fpd_query').focus() ;
+	$('#fpd_form').submit ( function () {
+		setTimeout ( function () { $('#fpd_results').html ( '<div class="alert alert-info">Querying database...</div>' ) ; } , 1 ) ;
+		var query = $('#fpd_query').val() ;
+		
+		var url = synbiota_data.api_url + '/api/' ;
+		if ( synbiota_data.api_version > 0 ) url += synbiota_data.api_version + '/' ;
+		url += 'parts?token=' + synbiota_data.token + '&name=' + escape ( query ) ;
+
+		$.getJSON ( gentle_config.proxy + '?callback=?' , 
+			{ url : url } ,
+			function ( data ) {
+				me.fpd_results = JSON.parse ( data ) ;
+				me.show_fpd_results() ;
+		} ) ;
+		
+		return false ;
+	} ) ;
+}
+
+synbiota.prototype.show_fpd_results = function () {
+	var me = this ;
+	var h = '' ;
+	h += "<table class='table'>" ;
+	h += "<tr><th>Kind</th><th>Name / Description</th></tr>" ;
+	
+	$.each ( me.fpd_results , function ( k , v ) {
+		h += "<tr id='fpd_result_" + k + "' class='fpd_result_row'>" ;
+		h += "<td>" + (v.kind||'Unknown') + "</td>" ;
+		h += "<td><div><b>" + (v.name||'<i>No name</i>') + "</b></div>" ;
+		h += "<div style='max-height:60px;overflow:auto;'><pre style='font-size:7pt;font-family:Verdana;line-height:100%;'>" + (v.description||'<i>No description available</i>') + "</pre></div>" ;
+		h += "</td>" ;
+		h += "</tr>" ;
+	} ) ;
+	
+	h += "</table>" ;
+	$('#fpd_results').html ( h ) ;
+	
+	$('.fpd_result_row').css ( { cursor : 'pointer' } ) ;
+
+	$.each ( me.fpd_results , function ( k , v ) {
+		$('#fpd_result_'+k).click ( function () { me.fpd_load_part ( k ) } ) ;
+/*		var s = ( v.description || '' ) ;
+		if ( s == '' ) return ;
+		$('#fpd_result_'+k).popover ( { content : "<pre>" + s + "</pre>" } ) ;*/
+	} ) ;
+}
+
+synbiota.prototype.fpd_load_part = function ( num ) {
+	var me = this ;
+	var o = me.fpd_results[num] ;
+	
+//	console.log ( o ) ;
+	if ( undefined === o.completed_part_id || null === o.completed_part_id ) { // Load part from project/id
+		synbiota_load_sequence_from_project ( o.project_id , o.id ) ;
+	} else { // Load completed part
+		synbiota_load_sequence_part ( o.completed_part_id ) ;
+	}
 }
 
 synbiota.prototype.global_init = function () {
@@ -73,7 +158,7 @@ synbiota.prototype.global_init = function () {
 	} else {
 		while ( gentle.sequences.length > 0 ) gentle.closeCurrentSequence() ;
 		synbiota_data.sequence_id = gentle.url_vars.id ;
-		synbiota_load_sequence ( gentle.url_vars.project_id , gentle.url_vars.id ) ;
+		synbiota_load_sequence_from_project ( gentle.url_vars.project_id , gentle.url_vars.id ) ;
 	}
 }
 
@@ -82,10 +167,21 @@ function synbiota () {
 }
 
 
-function synbiota_load_sequence ( project_id , sequence_id ) {
+function synbiota_load_sequence_from_project ( project_id , sequence_id ) {
 	var url = synbiota_data.api_url + '/api/' ;
 	if ( synbiota_data.api_version > 0 ) url += synbiota_data.api_version + '/' ;
 	url += 'projects/'+project_id+'/gentle_files/'+sequence_id+'?token='+synbiota_data.token ;
+	synbiota_load_sequence ( url ) ;
+}
+
+function synbiota_load_sequence_part ( part_id ) {
+	var url = synbiota_data.api_url + '/api/' ;
+	if ( synbiota_data.api_version > 0 ) url += synbiota_data.api_version + '/' ;
+	url += 'parts/' + part_id + '?token='+synbiota_data.token ;
+	synbiota_load_sequence ( url ) ;
+}
+
+function synbiota_load_sequence ( url ) {
 	$.getJSON ( gentle_config.proxy + '?callback=?' , 
 		{ url : url } ,
 		function ( data ) {
