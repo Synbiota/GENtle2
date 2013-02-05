329e06356e20bd452ad80bd1a1b3ae6a3a184b8d
Magnus Manske <magnusmanske@googlemail.com>
2013-02-05 23:18:19 +0000

    bug fix: no context menu for selection when sequence has no annotation at all

--- util/sequence/sequences.js
+++ util/sequence/sequences.js
@@ -50,6 +50,7 @@ Sequence.prototype.getFeaturesInRange = function ( from , to ) {
 	var ret = {} ;
 	$.each ( me.features , function ( fid , f ) {
 		if ( undefined === f['_range'] ) return ;
+		if ( f['_range'].length == 0 ) return ;
 		if ( to+1 < f['_range'][0].from ) return ;
 		if ( from+1 > f['_range'][f['_range'].length-1].to ) return ;
 		ret[fid] = f ;
