4e3ad767baa514a289a7bd5d517e03a850a6ae70
Dave Kerr <dave.kerr@gmail.com>
2013-05-17 17:46:19 +0100

    Made proxy.php callback conditional

--- data/proxy.php
+++ data/proxy.php
@@ -5,7 +5,10 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
 $url = $_REQUEST['url'] ;
-$callback = $_REQUEST['callback'] ;
+if($_REQUEST['callback'])
+{
+  $callback = $_REQUEST['callback'];
+}
 header('Content-type: application/json');
 print $callback . "(" . json_encode ( file_get_contents ( $url ) ) . ");" ;
 
