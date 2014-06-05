209535eeab651375318c0a10af468b48f5609231
husenxce <miralihussainkhan@gmail.com>
2014-06-05 19:34:59 -0400

    added edit tab files

--- public/scripts/sequence/views/edit_view.js
+++ public/scripts/sequence/views/edit_view.js
@@ -3,44 +3,39 @@
 @module Sequence
 @submodule Views
 **/
-define(function (require) {
-    var template = require('hbars!sequence/templates/edit_settings_view'),
+define(function(require) {
+    var template = require('hbars!sequence/templates/edit_view'),
         Backbone = require('backbone.mixed'),
         Gentle = require('gentle')(),
-        ls = require('localstorage'),
         Sequences = require('sequence/models/sequences'),
         EditView;
 
     EditView = Backbone.View.extend({
-
         manage: true,
-
         template: template,
 
-
-        initialize: function () {
-
-            this.model = Gentle.currentSequence;
+        initialize: function() {
+        this.model = Gentle.currentSequence;
+        this.collections = Sequences;
         },
 
         events: {
-            'click .sequence-save-button': 'readinfo',
+            'click .sequence-save-button': 'updateNameDescription',
         },
 
-        readinfo: function(event){
-        
+        updateNameDescription: function(event) {
+
+            this.model.set('name',this.$('#name').val());
+            this.model.set('description',this.$('#desc').val());
+            this.model.sync('update',this.model);
+
         },
 
-        updateNameDescription: function () {
-
-        },
-
-
-        serialize: function(){
-        return {         
-         Name : this.model.name,
-         Desc : this.model.description
-        };
+        serialize: function() {
+            return {
+                Name: this.model.get('name'),
+                Desc: this.model.get('description')
+            };
         }
 
     });
