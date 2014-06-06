ba2c440cfb52771a8d355697ba29ed2e25a59528
husenxce <miralihussainkhan@gmail.com>
2014-06-05 22:36:56 -0400

    added edit tab files

--- public/scripts/sequence/views/edit_view.js
+++ public/scripts/sequence/views/edit_view.js
@@ -15,29 +15,29 @@ define(function(require) {
         template: template,
 
         initialize: function() {
-        this.model = Gentle.currentSequence;
-        this.collections = Sequences;
+            this.model = Gentle.currentSequence;
         },
 
         events: {
-            'click .sequence-save-button': 'updateNameDescription',
+            'click input[type=submit]': 'updateNameDescription',
         },
 
         updateNameDescription: function(event) {
-
-            this.model.set('name',this.$('#name').val());
-            this.model.set('description',this.$('#desc').val());
-            this.model.sync('update',this.model);
-
+            this.model.set('name', this.$('#name').val() || 'Unnamed');
+            this.model.set('description', this.$('#desc').val() || 'No Description');
+            this.model.sync('update', this.model);
+            document.title = this.model.get('name');
         },
 
         serialize: function() {
+
             return {
+
                 Name: this.model.get('name'),
                 Desc: this.model.get('description')
             };
-        }
 
+        }
     });
     return EditView;
 
