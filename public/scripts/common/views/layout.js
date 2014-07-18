caf2992b3e5990a92d9fd58d554d26fa8e88ae00
husenxce <miralihussainkhan@gmail.com>
2014-07-18 19:43:05 -0400

    Added code for dragging and dropping files into the browser

--- public/scripts/common/views/layout.js
+++ public/scripts/common/views/layout.js
@@ -6,6 +6,8 @@ define(function(require) {
   var template      = require('hbars!common/templates/layout'),
       NavbarView    = require('common/views/navbar_view'),
       Backbone      = require('backbone.mixed'),
+      Filetypes   = require('common/lib/filetypes/filetypes'),
+      Gentle      = require('gentle')(),
       Layout;
 
   Layout = Backbone.Layout.extend({
@@ -14,8 +16,42 @@ define(function(require) {
 
     initialize: function() {
       this.setView('#navbar', new NavbarView());
+
     },
 
+    events:{
+    'dragover': 'cancelEvent',
+    'dragenter':'cancelEvent',
+    'drop'     :'loadFiles'
+    },
+
+  cancelEvent: function(event){
+  if (event.preventDefault) {
+      event.preventDefault();
+    }
+    },
+
+  loadFiles: function(event){
+  if (event.preventDefault) event.preventDefault(); 
+
+  var files = event.originalEvent.dataTransfer.files;
+  var onLoad = function(result) {
+   Filetypes.guessTypeAndParseFromArrayBuffer(result.content, result.name).then ( function ( sequences ) {
+    if ( sequences.length ) Gentle.addSequencesAndNavigate(sequences);
+      else alert('Could not parse the sequence.');
+     } , function (err) {
+       console.log(err);
+       alert('Could not parse the sequence.');
+     } ) ;
+  };
+  var onError = function(filename) {
+    alert('Could not load file ' + filename);
+  };
+
+  _.each(files, function(file) {
+    Filetypes.loadFile(file,true).done(onLoad, onError);
+  });
+  }
   });
 
   return Layout;
