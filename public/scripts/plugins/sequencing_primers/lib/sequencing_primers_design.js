8741d7f4d24823e0cfca88e2244716ddd04c10a6
Alexander James Phillips <ajp.global@gmail.com>
2015-03-02 15:01:18 +0000

    Choose and render correct final primer for Sequence Primer mode
    This is an implementation.  It is brittle.  We should be passing around a full
    sequence model with its relationship to the full original sequence it has
    come from so that primer position and offsets are calculated correctly.
    For example, at the moment, the overlap is just subtracted from the length
    of the sequence the primer pair were produced against, however this length
    can and will change as the overlap will not be constant.

--- public/scripts/plugins/sequencing_primers/lib/sequencing_primers_design.js
+++ public/scripts/plugins/sequencing_primers/lib/sequencing_primers_design.js
@@ -3,14 +3,26 @@ import SequenceTransforms from '../../../sequence/lib/sequence_transforms';
 import _ from 'underscore.mixed';
 import Q from 'q';
 
-var chunkLength = 500;
+var maxChunkLength = 500;
 var overlap = 30;
 
 var splitSequence = function(sequence) {
   var output = [];
+  var len = sequence.length;
 
-  for(var i = 0; i < sequence.length; i += chunkLength - overlap) {
-    output.push(sequence.substr(i, chunkLength));
+  // Calculate number of chunks
+  var x = Math.ceil(len/maxChunkLength);
+  var q = x * (maxChunkLength - overlap) + Math.max((x-1),0) * overlap;
+  var numberOfChunks = Math.ceil((len * x) / q);
+  var start;
+  var halfOverlap = Math.ceil(overlap/2);
+  var lenWithoutOverlap = Math.ceil(len / numberOfChunks);
+
+  for(var i = 0; i < numberOfChunks; i += 1) {
+    var isNotFirst = (i !== 0 ? 1 : 0);
+    start = (i * lenWithoutOverlap) - (halfOverlap * isNotFirst);
+    var partOfSequence = sequence.substr(start, (lenWithoutOverlap + halfOverlap));
+    output.push(partOfSequence);
   }
 
   return output;
@@ -42,11 +54,12 @@ var getPrimersPair = function(sequence) {
 
       resolve({
         forwardPrimer: _.extend(forwardPrimer, {sequenceLength: forwardPrimer.sequence.length}),
-        reversePrimer: _.extend(reversePrimer, {sequenceLength: reversePrimer.sequence.length})
+        reversePrimer: _.extend(reversePrimer, {sequenceLength: reversePrimer.sequence.length}),
+        fullSequence: sequence,
       });
 
     }, (e) => console.log(e));
-  
+
 
   }, (e) => console.log(e));
 };
@@ -61,7 +74,7 @@ var getAllPrimers = function(sequence) {
   var sequenceChunks = splitSequence(sequence);
   var maxParallel = 5;
   var numberBatches = Math.ceil(sequenceChunks.length / maxParallel);
-  var lastProgress = _.map(_.range(numberBatches), function() { return {}; });
+  // var lastProgress = _.map(_.range(numberBatches), function() { return {}; });
 
   return Q.promise(function(resolve, reject, notify) {
 
@@ -71,47 +84,33 @@ var getAllPrimers = function(sequence) {
 
       if(i > numberBatches) return Q(results);
 
-      return Q.all(promises).progress(function(current) {
-        lastProgress[current.index] = current.value;
-        // notify(aggregateProgress(lastProgress));
-      }).then(function(results_) {
+      return Q.all(promises
+      // ).progress(function(current) {
+      //   lastProgress[current.index] = current.value;
+      //   notify(aggregateProgress(lastProgress));
+      // }
+      ).then(function(results_) {
         notify(i/numberBatches);
         return getParallelPrimers(i + maxParallel, results.concat(results_));
       }, (e) => console.log(e));
     };
-  
+
     return getParallelPrimers().then(function(primers) {
+      var total = 0;
       resolve(_.map(primers, function(primer, i) {
-        var from =  Math.max(0, i * chunkLength - (i) * overlap);
+        var from = total;
+        total += (primer.fullSequence.length - overlap);  // This smells brittle
+        var to = total + overlap;
         return _.extend(primer, {
           from: from,
           index: i,
-          to: from + chunkLength,
+          to: to,
           id: _.uniqueId()
         });
       }));
     }, (e) => console.log(e));
 
-
-
   }, (e) => console.log(e));
-
-  
-
-
-
-
-  // Q.all(_.map(sequenceChunks, getPrimersPair)).then(function(results) {
-  //   console.log(results);
-  // }, (e) => console.log(e));
-
-
-
-
-
-  // getPrimersPair(sequenceChunks[0]).then(function(results) {
-  //   console.log('results', results)
-  // }, (e) => console.log(e));
 };
 
 export default getAllPrimers;
