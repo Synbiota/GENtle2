92737fb207e8f35dc3fcd6aa888b2550ebb8e31e
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-03-04 15:27:46 +0000

    Designer mode wording

--- public/scripts/plugins/designer/plugin.js
+++ public/scripts/plugins/designer/plugin.js
@@ -5,7 +5,7 @@ define(function(require) {
 
   Gentle.addPlugin('sequence-primary-view', {
     name: 'designer',
-    title: 'Combine sequences',
+    title: 'Assemble sequences',
     view: DesignerView
   });
 
