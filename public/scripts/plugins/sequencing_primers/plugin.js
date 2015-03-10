9a80037ad437b390ea670de938bad6f1a8253228
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-03-10 17:19:29 +0100

    Hiding sequencing primers by default

--- public/scripts/plugins/sequencing_primers/plugin.js
+++ public/scripts/plugins/sequencing_primers/plugin.js
@@ -4,5 +4,6 @@ import View from './views/sequencing_primers_view';
 Gentle.addPlugin('sequence-primary-view', {
   name: 'sequencing_primers',
   title: 'Sequencing primers mode',
-  view: View
+  view: View,
+  visible: Gentle.featureFlag('sequencingPrimers')
 });
