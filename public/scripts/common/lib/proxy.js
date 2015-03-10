11bee8a42676ab436536112cb4ba9c780c95865f
Alexander James Phillips <ajp.global@gmail.com>
2015-03-10 14:59:27 +0000

    Add simple retry and backoff to YQL Query

--- public/scripts/common/lib/proxy.js
+++ public/scripts/common/lib/proxy.js
@@ -61,8 +61,7 @@ export default {
       query + ' ' + whereClause;
 
     if(method !== 'POST') query = encodeURIComponent(query);
-
-    return Q($.ajax({
+    var queryObject = {
       url:  'https://query.yahooapis.com/v1/public/yql',
       type: method,
       dataType: format,
@@ -70,7 +69,43 @@ export default {
         q: query,
         env: 'http://datatables.org/alltables.env',
         format: format
+    }};
+
+    // Retry code
+    var deferredYqlQuery = Q.defer();
+    this._retryableYqlQuery(queryObject, deferredYqlQuery, this.MAX_RETRIES);
+
+    return deferredYqlQuery.promise;
+  },
+
+  MAX_RETRIES: 3,
+
+  _retryableYqlQuery: function(queryObject, deferredYqlQuery, triesLeft) {
+    console.log(`yqlQuery (${triesLeft}):`, queryObject.data.q.replace("SELECT * FROM xml WHERE url='http://www.idtdna.com/AnalyzerService/AnalyzerService.asmx/Analyze?Sequence", ''));
+
+    var ajaxQuery = $.ajax(queryObject);
+
+    Q(ajaxQuery).then((response)  => {
+      if(triesLeft < this.MAX_RETRIES) {
+        console.log('We had to retry but...');
       }
-    }));
+      console.log('we have an answer', response, queryObject.data.q.replace("SELECT * FROM xml WHERE url='http://www.idtdna.com/AnalyzerService/AnalyzerService.asmx/Analyze?Sequence", ''));
+      deferredYqlQuery.resolve(response);
+    }).catch((e) => {
+      triesLeft -= 1;
+      var msg = `YQL query encounted ${e.status} error: ${e.statusText}.  RetriesLeft: ${triesLeft}.`;
+      if(triesLeft) {
+        // Retry
+        var seconds = 2 * Math.pow(this.MAX_RETRIES - triesLeft, 2);
+        console.warn(msg + `  Retrying in ${seconds} seconds.`);
+        setTimeout(() => {
+          this._retryableYqlQuery(queryObject, deferredYqlQuery, triesLeft);
+        }, seconds * 1000);
+      } else {
+        // Fail
+        console.error(msg + `  Full response: ${e.responseText}`);
+        deferredYqlQuery.reject(e);
+      }
+    });
   }
 };
