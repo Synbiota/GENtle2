22fa5e051816560d83204629a571767eeb3ce59b
Alexander James Phillips <ajp.global@gmail.com>
2015-03-25 17:30:18 +0000

    Transform old pcr product stickyEnd data to new format returned from UI

--- public/scripts/plugins/pcr/lib/utils.js
+++ public/scripts/plugins/pcr/lib/utils.js
@@ -1,17 +1,46 @@
 import TemporarySequence from '../../../sequence/models/temporary_sequence';
+import stickyEnds from '../../../common/lib/sticky_ends';
+
+
+var transformStickyEndData = function(stickyEndAttributes) {
+  var newStickyEndAttributes;
+  if(_.isString(stickyEndAttributes.start)) {
+    // We're dealing with an old stickyEnd.  Try to transform it into the
+    // new form
+    var matchedStickyEnds = _.filter(stickyEnds, (stickyEnd) => {
+      return (
+        stickyEnd.start.sequence === stickyEndAttributes.start &&
+        stickyEnd.end.sequence === stickyEndAttributes.end &&
+        stickyEnd.start.offset === stickyEndAttributes.startOffset &&
+        stickyEnd.end.offset === (-stickyEndAttributes.endOffset)
+      );
+    });
+    if(matchedStickyEnds.length !== 1) {
+      throw `Could not transform old stickyEnd into new. "${JSON.stringify(stickyEndAttributes)}"  did not match 1 and only 1 sticky ends.  "${JSON.stringify(matchedStickyEnds)}"`;
+    } else {
+      newStickyEndAttributes = _.deepClone(matchedStickyEnds[0]);
+    }
+  } else {
+    newStickyEndAttributes = stickyEndAttributes;
+  }
+  return newStickyEndAttributes;
+};
 
 
 var getPcrProductsFromSequence = function(sequenceModel) {
   var attributesOfPcrProducts = sequenceModel.get('meta.pcr.products') || [];
 
-  //Backwards compatibility.  Some of the pcr products were stored without the sequence attribute calculated.
   attributesOfPcrProducts = _.each(attributesOfPcrProducts, (productAttributes) => {
+    //Backwards compatibility.  Some of the pcr products were stored without the sequence attribute calculated.
     if(!productAttributes.sequence) {
       var sequenceNts = sequenceModel.get('sequence');
       var opts = _.pick(productAttributes, ['from', 'to', 'stickyEnds']);
       var {productSequence: productSequenceNts} = TemporarySequence.calculateSequence(sequenceNts, opts);
       productAttributes.sequence = productSequenceNts;
     }
+
+    //Backwards compatibility.  Some of the pcr products were stored with incomplete stickyEnd data.
+    productAttributes.stickyEnds = transformStickyEndData(productAttributes.stickyEnds);
   });
 
   var products = _.map(attributesOfPcrProducts, (productAttributes) => new TemporarySequence(productAttributes));
@@ -27,6 +56,44 @@ var savePcrProductsToSequence = function(sequenceModel, products) {
   var attributesOfPcrProducts = JSON.parse(JSON.stringify(products));
   return sequenceModel.set('meta.pcr.products', attributesOfPcrProducts).throttledSave();
 };
+
+
+// Testing
+if(false) {
+  var getOldStickyEndAttributes = function() {
+    return {
+      name: "X-Z'",
+      startName: "X",
+      endName: "Z'",
+      start: "CCTGCAGTCAGTGGTCTCTAGAG",
+      end: "GAGATGAGACCGTCAGTCACGAG",
+      startOffset: 19,
+      endOffset: -19
+    };
+  };
+  var getExpectedStickyEndAttributes = function() {
+    return {
+      name: "X-Z'",
+      start: {
+        sequence: 'CCTGCAGTCAGTGGTCTCTAGAG',
+        reverse: false,
+        offset: 19,
+        size: 4,
+        name: "X",
+      },
+      end: {
+        sequence: 'GAGATGAGACCGTCAGTCACGAG',
+        reverse: true,
+        offset: 19,
+        size: 4,
+        name: "Z'",
+      }
+    };
+  };
+
+  var transformedStickyEndData = transformStickyEndData(getOldStickyEndAttributes());
+  console.assert(_.isEqual(transformedStickyEndData, getExpectedStickyEndAttributes()));
+}
 
 
 export default {getPcrProductsFromSequence, savePcrProductsToSequence};
