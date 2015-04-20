eb550547b3ef05ffa8ebb194f32b7d05e1cd3433
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-04-20 10:19:30 +0100

    Reverted designer to version before refactor (92737fb20)
    - public/scripts/plugins/designer/*
    - public/stylesheets/views/_designer_view.scss

--- public/scripts/plugins/designer/views/available_sequence_view.js
+++ public/scripts/plugins/designer/views/available_sequence_view.js
@@ -3,7 +3,6 @@ define(function(require) {
       template = require('../templates/available_sequence_view.hbs'),
       SynbioData = require('../../../common/lib/synbio_data'),
       Gentle = require('gentle'),
-      draggableCleanup = require('../lib/draggable_cleanup'),
       AvailableSequenceView;
 
   AvailableSequenceView = Backbone.View.extend({
@@ -14,7 +13,9 @@ define(function(require) {
 
     initialize: function(){
       this.listenTo(Gentle.currentSequence, 'change', this.render, this);
+      this.model = Gentle.currentSequence;
       this.features = [];
+      this.availableSequences = Gentle.sequences.without(this.model);
     },
 
     processFeatures: function() {
@@ -46,17 +47,15 @@ define(function(require) {
         return feature.from;
       });
 
-      var sequenceId = this.model.get('id');
       this.sequenceInfo = {
         name: this.model.get('name'),
-        id: sequenceId,
+        id: 0,
         from: 0,
         to: this.model.length()-1,
         length: this.model.length(),
         type: 'Sequence',
         features: this.model.get('features'),
-        // hidden: this.model.maxOverlappingFeatures()>1,
-        usable: this.parentView().isInsertable(this.model),
+        hidden: this.model.maxOverlappingFeatures()>1
       };
     },
 
@@ -68,102 +67,64 @@ define(function(require) {
           maxOverlapStackIndex = 0, length,
           $featuresElem;
 
-      for(var i = 0; i < this.features.length; i++) {
+       for(var i = 0; i < this.features.length; i++) {
         feature = this.features[i];
-        var frm = feature.from;
-        var to = feature.to;
-        if(frm>to){
-          to = frm;
-          frm = feature.to;
-        }
         featureWidth = Math.max(
-          Math.floor((to - frm + 1) / maxBase * viewWidth), 
+          Math.floor((feature.to - feature.from + 1) / maxBase * viewWidth), 
           this.minFeatureWidth
         );
-        $featureElement = this.$('[data-feature_id="'+feature.id+'"]');
+        $featureElement = this.$('[data-feature-id="'+feature.id+'"]');
 
         $featureElement.css({
-          left: Math.floor(frm / maxBase * viewWidth),
+          left: Math.floor(feature.from / maxBase * viewWidth),
           width: featureWidth
         });
 
         overlapIndex = overlapStack.length;
 
         for(var j = overlapStack.length - 1; j >= 0; j--) {
-          if(overlapStack[j] === undefined || overlapStack[j][1] <= frm) {
+          if(overlapStack[j] === undefined || overlapStack[j][1] <= feature.from) {
             overlapStack[j] = undefined;
             overlapIndex = j;
           }
         }
 
         $featureElement.addClass('designer-available-sequence-feature-stacked-'+overlapIndex);
-        overlapStack[overlapIndex] = [frm, to];
+        overlapStack[overlapIndex] = [feature.from, feature.to];
         maxOverlapStackIndex = Math.max(maxOverlapStackIndex, overlapStack.length);
       }
-
       $featuresElem = this.$('.designer-available-sequence-features');
       $featuresElem.addClass('designer-available-sequence-features-max-overlap-' + maxOverlapStackIndex);
     },
 
-    showAnnotations: function() {
-      return Gentle.currentUser.get('displaySettings.designerView.showAnnotations') || false;
-    },
-
     serialize: function() {
-      var showAnnotations = this.showAnnotations();
       this.processFeatures();
       return {
         sequence: this.sequenceInfo,
-        descriptiveAnnotationContent: this.parentView().getDescriptiveAnnotationContent(this.model),
-        features: this.features,
-        showAnnotations: showAnnotations,
+        features: this.features
       };
     },
 
     afterRender: function() {
+      var _this = this;
       this.positionFeatures();
 
-      // this.$('.designer-available-sequence-feature').draggable({
-      //   refreshPositions: true,
-      //   revert: 'invalid',
-      //   helper: 'clone',
-      //   cursorAt: {
-      //     top: 5,
-      //     left: 5
-      //   }
-      // });
-      var sequenceId = this.model.get('id');
-      this.$('.designer-available-sequence-entireseq').draggable({
-        refreshPositions: true,
+      this.$('.designer-available-sequence-feature').draggable({
         revert: 'invalid',
         helper: 'clone',
         cursorAt: {
           top: 5,
           left: 5
         }
-      }).hover(
-      (event) => {
-        this.parentView().hoveredOverSequence(sequenceId);
-      },
-      (event) => {
-        this.parentView().unhoveredOverSequence(sequenceId);
       });
-    },
-
-    cleanUpDraggable: function() {
-      draggableCleanup(
-        this, 
-        '.designer-available-sequence-entireseq'
-      );
-    },
-
-    beforeRender: function() {
-      this.cleanUpDraggable();
-    },
-
-    remove: function() {
-      this.cleanUpDraggable();
-      Backbone.View.prototype.remove.apply(this, arguments);
+       this.$('.designer-available-sequence-entireseq').draggable({
+        revert: 'invalid',
+        helper: 'clone',
+        cursorAt: {
+          top: 5,
+          left: 5
+        }
+      });
     },
 
   });
--- public/scripts/plugins/designer/views/home_designer_view.js
+++ public/scripts/plugins/designer/views/home_designer_view.js
@@ -3,82 +3,37 @@
 @submodule Views
 @class HomeDesignerView
 **/
-import Backbone from 'backbone';
-import template from '../templates/home_designer_view.hbs';
-import Sequence from '../../../sequence/models/sequence';
-import Filetypes from '../../../common/lib/filetypes/filetypes';
-import Gentle from 'gentle';
-import Q from 'q';
-import _ from 'underscore';
+define(function(require) {
+  var Backbone    = require('backbone'),
+      template    = require('../templates/home_designer_view.hbs'),
+      Sequence    = require('../../../sequence/models/sequence'),
+      Gentle      = require('gentle'),
+      HomeDesignerView;
 
-export default Backbone.View.extend({
-  manage: true,
-  template: template,
-  className: 'home-designer',
+  HomeDesignerView = Backbone.View.extend({
+    manage: true,
+    template: template,
+    className: 'home-designer',
 
-  events: {
-    'submit .home-designer-form': 'clickInputElement',
-    'change .home-designer-form input[name=file]': 'openSequenceFromFile',
-  },
-  
-  createNewSequence: function(event, loadedSequences) {
-    event.preventDefault();
-    var $form     = this.$('form').first(),
-        name      = $form.find('[name=name]').val() || 'Unnamed',
-        sequence  = new Sequence({
-          name: name,
-          sequence: '',
-          displaySettings: {
-            primaryView: 'designer'
-          },
-          meta: {
-            designer: {
-              sequences: loadedSequences, 
-            },
-          }
-          
-        });
+    events: {
+      'submit .home-designer-form': 'createNewSequence',
+    },
 
-    Gentle.addSequencesAndNavigate([sequence]);
-  },
+    createNewSequence: function(event) {
+      event.preventDefault();
+      var $form     = this.$('form').first(),
+          name      = $form.find('[name=name]').val() || 'Unnamed',
+          sequence  = new Sequence({
+            name: name,
+            sequence: '',
+            displaySettings: {
+              primaryView: 'designer'
+            }
+          });
 
-  openSequenceFromFile: function(event) {
-    event.preventDefault();
-    var $form     = $('.home-designer-form').first(),
-        input     = $form.find('input[name=file]')[0],
-        loadedSequences = [];
+      Gentle.addSequencesAndNavigate([sequence]);
+    }
+  });
 
-    var onLoad = function(result) {
-      return Filetypes.guessTypeAndParseFromArrayBuffer(result.content, result.name).then ( function ( sequences ) {
-        if ( sequences.length ) {
-          loadedSequences = loadedSequences.concat(sequences);
-        } else{
-         alert('Could not parse the sequence.');
-        }
-      }, function (err) {
-        console.log(err);
-        alert('Could not parse the sequence.');
-      });
-    };
-
-    var onError = function(filename) {
-      alert('Could not load file ' + filename);
-    };
-
-    var onLoadPromises = _.map(input.files, function(file) {
-      return Filetypes.loadFile(file,true).then(onLoad, onError);
-    });
-
-    Q.all(onLoadPromises).then(() => {
-      this.createNewSequence(event, loadedSequences);
-    }).done();
-
-  },
-
-  clickInputElement: function(event) {
-    event.preventDefault();
-    this.$('.home-designer-form input[name=file]').click();
-  }
-
- 
+  return HomeDesignerView;
 });
