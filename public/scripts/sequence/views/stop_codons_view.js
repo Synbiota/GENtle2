51b06a9ec05b32e1e7cf08720b94039ddcfc93ad
Dave Russell <dave.kerr@gmail.com>
2015-05-01 10:49:33 +0100

    wip

---
+++ public/scripts/sequence/views/stop_codons_view.js
@@ -0,0 +1,21 @@
+import _ from 'underscore';
+import Backbone from 'backbone';
+import Gentle from 'gentle';
+import SynbioData from '../../common/lib/synbio_data';
+import template from ',,/templates/stop_codons_view.hbs';
+
+
+export default Backbone.view.extend({
+
+  template: template,
+  manage: true,
+
+  events: {
+
+  },
+
+  initialize: function() {
+    
+  }
+
+});
