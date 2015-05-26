a6c1d6fd507c0e7192e901d2a22642691b1e2b40
Alexander James Phillips <ajp.global@gmail.com>
2015-05-26 18:18:49 +0100

    log deprecation warning when running tests

--- public/scripts/common/lib/deprecated.js
+++ public/scripts/common/lib/deprecated.js
@@ -1,8 +1,17 @@
+
 function deprecatedMethod(obj, methodName, replacement) {
   var constructorName = obj.displayName || obj.constructor.name;
-  console.groupCollapsed("%c DEP ", "background-color: cyan; color: blue;", `Using ${methodName} on ${constructorName} instances is deprecated. Use ${replacement} instead.`);
-  console.log((new Error()).stack);
-  console.groupEnd();
+  var message = `Using ${methodName} on ${constructorName} instances is deprecated. Use ${replacement} instead.`;
+  var stack = (new Error()).stack;
+  if(stack) {
+    console.groupCollapsed("%c DEP ", "background-color: cyan; color: blue;", message);
+    console.log(stack);
+    console.groupEnd();
+  } else {
+    // This is needed for tests in PhantomJS where the stack is otherwise empty
+    // and the error message is not displayed.
+    console.warn(message);
+  }
 }
 
 export default deprecatedMethod;
