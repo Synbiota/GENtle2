318c6f5535d4f08e76606dc10d7066d336571b5c
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-05-26 14:55:16 +0100

    Re-added range class

---
+++ public/scripts/library/sequence-model/range.js
@@ -0,0 +1,11 @@
+class Range {
+  from: number;
+  size: number;
+  reverse: boolean;
+  constructor({from, size, reverse = false}) {
+    this.from = from;
+    this.size = size;
+    this.reverse = reverse;
+  }
+  get to() { return this.from + this.size; }
+}
