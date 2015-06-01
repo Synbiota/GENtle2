d0188cb712976bfbbc6b13b436c91cecf6d810c3
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-06-01 11:42:16 +0200

    Added bugsnag exception handling

--- tasks/utils/browserify_utils.js
+++ tasks/utils/browserify_utils.js
@@ -1,10 +1,15 @@
 var aliases = require('./javascript_aliases');
+var isDev = process.env.NODE_ENV !== 'production';
 
 var transforms = [
   [ 'hbsfy', {compiler: 'require("handlebars.mixed");'} ],
   [ 'babelify', {} ],
   [ 'aliasify', {aliases: aliases} ]
 ];
+
+if(!isDev) {
+  transforms.push([ 'envify', {} ]);
+}
 
 module.exports = {
   transforms: transforms,
