8c3e1585bcc3ba42a13ff838cb0c52616decb555
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-06-01 13:19:47 +0200

    Using node-env-file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var fs = require('fs');
+var envFile = __dirname + '/app.env';
 
+if(fs.existsSync(envFile)) {
+  require('node-env-file')(__dirname + '/app.env');
+}
 
 require('./tasks/javascript');
 require('./tasks/css');
