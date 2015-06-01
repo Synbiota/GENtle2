834f261149069097edc8e82a6a93aefa01075613
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-06-01 14:57:48 +0200

    Fixed envify configuration to handle opsworks syntax for app.env file

--- tasks/utils/browserify_utils.js
+++ tasks/utils/browserify_utils.js
@@ -3,6 +3,19 @@ var isDev = process.env.NODE_ENV !== 'production';
 var envify = require('envify/custom');
 var _ = require('underscore');
 
+var envVarKeys = [
+  'ENABLE_BUGSNAG',
+  'BUGSNAG_API_KEY',
+  'BUGSNAG_RELEASE_STAGE'
+];
+
+// Hack to work around Opsworks weird app.env syntax
+var pickedEnvVars = envVarKeys.reduce(function(memo, key) {
+  var envVar = process.env['export "'+key+'"'];
+  if(envVar) memo[key] = envVar;
+  return memo;
+}, {});
+
 var transforms = [
   [ 'hbsfy', {compiler: 'require("handlebars.mixed");'} ],
   [ 'babelify', {} ],
@@ -10,14 +23,11 @@ var transforms = [
 ];
 
 console.log('ENVIRONMENT VARIABLES', _.keys(process.env))
+console.log('PICKED ENVIRONMENT VARIABLES', pickedEnvVars)
 
 if(!isDev) {
   transforms.push(
-    [ envify(_.pick(process.env, 
-      'ENABLE_BUGSNAG',
-      'BUGSNAG_API_KEY',
-      'BUGSNAG_RELEASE_STAGE'
-    )), {} ],
+    [ envify(pickedEnvVars), {} ],
     [ 'uglifyify', { global: true } ]
   );
 }
