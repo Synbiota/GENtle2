a6926249a90c1c3136ce55785b0fff6144e368e1
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-06-02 15:20:35 +0200

    Added bugsnag (serverside)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,4 @@
 var gulp = require('gulp');
-var fs = require('fs');
-var envFile = __dirname + '/app.env';
-
-if(fs.existsSync(envFile)) {
-  require('node-env-file')(__dirname + '/app.env');
-}
 
 require('./tasks/javascript');
 require('./tasks/css');
