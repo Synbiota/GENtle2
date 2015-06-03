e0bb89102476cffdd78ed6482005a90a76b20955
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-06-03 15:37:02 +0200

    Split bundle between app.min.js and vendor.js

--- tasks/utils/browserify_utils.js
+++ tasks/utils/browserify_utils.js
@@ -3,24 +3,62 @@ var envify = require('envify/custom');
 var appEnv = require('./import_app_env');
 var isDev = process.env.NODE_ENV !== 'production';
 
-var transforms = [
+var vendorLibs = [
+  'jquery',
+  'backbone',
+  'underscore',
+  'bootstrap',
+  'handlebars',
+  'backbone-deep-model',
+  'backbone.layoutmanager',
+  'backbone.localstorage',
+  'bugsnag-js',
+  'filesaver.js',
+  'jquery-ui',
+  'jquery-ui-touch-punch',
+  'q',
+  'underscore-deep-extend'
+]
+
+var appTransforms = [
   [ 'hbsfy', {compiler: 'require("handlebars.mixed");'} ],
   [ 'babelify', {} ],
   [ 'aliasify', {aliases: aliases} ]
 ];
 
+var vendorTransforms = [];
+
 if(!isDev) {
-  transforms.push(
+  appTransforms.push(
     [ envify(appEnv), {} ],
+    [ 'uglifyify', { global: true } ]
+  );
+
+  vendorTransforms.push(
     [ 'uglifyify', { global: true } ]
   );
 }
 
 module.exports = {
-  transforms: transforms,
-  applyTransforms: function(browserified) {
-    return transforms.reduce(function(b, t) {
+  appTransforms: appTransforms,
+  vendorTransforms: vendorTransforms,
+  vendorLibs: vendorLibs,
+
+  applyConfig: function(bundleType, browserified) {
+    var transforms;
+
+    if(bundleType === 'app') {
+      browserified.external(vendorLibs);
+      transforms = appTransforms;
+    } else {
+      browserified.require(vendorLibs);
+      transforms = vendorTransforms;
+    }
+
+    browserified = transforms.reduce(function(b, t) {
       return b.transform(t[0], t[1]);
     }, browserified);
+
+    return browserified;
   }
 };
