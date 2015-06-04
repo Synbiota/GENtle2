52ef675dc082408a2e40f5945d5a176269c63311
Alexander James Phillips <ajp.global@gmail.com>
2015-06-04 13:36:04 +0100

    Simplify finding universal primers

--- public/scripts/plugins/sequencing_primers/tests/universal_primers_spec.js
+++ public/scripts/plugins/sequencing_primers/tests/universal_primers_spec.js
@@ -1,25 +1,34 @@
-import {findPrimers, universalPrimers} from '../lib/universal_primers';
+import {stubCurrentUser} from '../../../common/tests/stubs';
 
+import {findPrimersHelper} from '../lib/universal_primers';
+import TemporarySequenceModel from '../../../sequence/models/temporary_sequence';
+
+
+stubCurrentUser();
 
 describe('finding universal primers', function() {
   it('finds reverse primer', function() {
     var onlyContainingReverseUniversalPrimer = 'GATCACTACCGGGCGTATT' + 'AAAAAAAAAA' + 'GATCACTACCGGGCGTATT';
-    var {forwardSequencePrimer, reverseSequencePrimer} = findPrimers(onlyContainingReverseUniversalPrimer, universalPrimers());
+    var sequenceModel = new TemporarySequenceModel({sequence: onlyContainingReverseUniversalPrimer});
+    var {forwardSequencePrimer, reverseSequencePrimer} = findPrimersHelper(sequenceModel);
 
     expect(forwardSequencePrimer).toBeUndefined();
 
-    expect(reverseSequencePrimer.from).toEqual(47);
-    expect(reverseSequencePrimer.to).toEqual(28);
-    expect(reverseSequencePrimer.reverse).toEqual(true);
+    expect(reverseSequencePrimer.range.from).toEqual(29);
+    expect(reverseSequencePrimer.range.size).toEqual(19);
+    expect(reverseSequencePrimer.range.reverse).toEqual(true);
+    expect(reverseSequencePrimer.range.to).toEqual(48);
   });
 
   it('finds forward primer', function() {
     var onlyContainingForwardUniversalPrimer = 'TGCCACCTGACGTCTAAGAA' + 'AAAAAAAAAA' + 'TGCCACCTGACGTCTAAGAA';
-    var {forwardSequencePrimer, reverseSequencePrimer} = findPrimers(onlyContainingForwardUniversalPrimer, universalPrimers());
+    var sequenceModel = new TemporarySequenceModel({sequence: onlyContainingForwardUniversalPrimer});
+    var {forwardSequencePrimer, reverseSequencePrimer} = findPrimersHelper(sequenceModel);
 
-    expect(forwardSequencePrimer.from).toEqual(0);
-    expect(forwardSequencePrimer.to).toEqual(19);
-    expect(forwardSequencePrimer.reverse).toEqual(false);
+    expect(forwardSequencePrimer.range.from).toEqual(0);
+    expect(forwardSequencePrimer.range.size).toEqual(20);
+    expect(forwardSequencePrimer.range.reverse).toEqual(false);
+    expect(forwardSequencePrimer.range.to).toEqual(20);
 
     expect(reverseSequencePrimer).toBeUndefined();
   });
@@ -37,15 +46,17 @@ describe('finding universal primers', function() {
       'GATCACTACCGGGCGTATT' +  // reverse universal primer v1                    // 19: 539-557  (558)
       'CG'                                                                       //  2: 558-559  (560)
     );
+    var sequenceModel = new TemporarySequenceModel({sequence: shortSequence});
+    var {forwardSequencePrimer, reverseSequencePrimer} = findPrimersHelper(sequenceModel);
 
-    var {forwardSequencePrimer, reverseSequencePrimer} = findPrimers(shortSequence, universalPrimers());
+    expect(forwardSequencePrimer.range.from).toEqual(41);
+    expect(forwardSequencePrimer.range.size).toEqual(20);
+    expect(forwardSequencePrimer.range.reverse).toEqual(false);
+    expect(forwardSequencePrimer.range.to).toEqual(61);
 
-    expect(forwardSequencePrimer.from).toEqual(41);
-    expect(forwardSequencePrimer.to).toEqual(60);
-    expect(forwardSequencePrimer.reverse).toEqual(false);
-
-    expect(reverseSequencePrimer.from).toEqual(557);
-    expect(reverseSequencePrimer.to).toEqual(538);
-    expect(reverseSequencePrimer.reverse).toEqual(true);
+    expect(reverseSequencePrimer.range.from).toEqual(539);
+    expect(reverseSequencePrimer.range.size).toEqual(19);
+    expect(reverseSequencePrimer.range.reverse).toEqual(true);
+    expect(reverseSequencePrimer.range.to).toEqual(558);
   });
 });
