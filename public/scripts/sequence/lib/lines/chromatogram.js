2ef03553b720726c7c7c4d5d026008c39338d492
Cliff <cliffdlau@gmail.com>
2015-06-04 19:10:10 -0400

    WIP implementation of new normalized chromatogram

--- public/scripts/sequence/lib/lines/chromatogram.js
+++ public/scripts/sequence/lib/lines/chromatogram.js
@@ -36,57 +36,101 @@ export default class Chromatogram extends Line {
     const xInterval =  1;
     const height = this.height;
 
-    const relevantRawData = _.map(rawData, function(data) {
-      // return data.slice(dataRelevantOffset + fromBase * dataDensity, dataRelevantOffset + toBase * dataDensity);
-      // return data.slice(dataDensity * fromBase, dataDensity * (toBase+1));
+    // const relevantRawData = _.map(rawData, function(data) {
+    //   // return data.slice(dataRelevantOffset + fromBase * dataDensity, dataRelevantOffset + toBase * dataDensity);
+    //   // return data.slice(dataDensity * fromBase, dataDensity * (toBase+1));
 
-      var start = fromBase === 0 ?
-                    0 :
-                    peaks[fromBase - 1],
+    //   var start = fromBase === 0 ?
+    //                 0 :
+    //                 peaks[fromBase - 1],
 
-          end   = toBase === peaks.length-1 ?
-                    data.length - 1 :
-                    peaks[toBase + 1];
+    //       end   = toBase === peaks.length-1 ?
+    //                 data.length - 1 :
+    //                 peaks[toBase + 1];
 
-          if (start > 0) start += layoutSettings.basePairDims.width/2;
-          if (end < peaks.length -1 ) end -= layoutSettings.basePairDims.width/2;
+    //       if (start > 0) start += layoutSettings.basePairDims.width/2;
+    //       if (end < peaks.length -1 ) end -= layoutSettings.basePairDims.width/2;
 
-      return data.slice(start, end+1);
-    });
-
-    var allPoints = _.map(relevantRawData, function(rawData_) {
-      return _.map(rawData_, function(data, i) {
-        // return [x + pageMargins.left + i * xInterval, y + height * ( 1 - data / maxDataValue)];
-        var xPos = x + i * xInterval,
-            yPos = y + height * ( 1 - data / maxDataValue);
-
-        // If we are writing a section of the sequence (not beginning) then assume that the x is positioned right
-        // after the previous nucleotide letter.
-        if (fromBase > 0){
-          xPos += layoutSettings.basePairDims.width/2;
-        }
-
-        return [xPos, yPos];
-      });
-    });
-
-    artist.updateStyle({
-      lineWidth: 1
-    });
-
-    // artist.rect(x, y, peaks[toBase]-peaks[fromBase], 100, {
-    //   fillStyle: "#"+((1<<24)*Math.random()|0).toString(16)
-    // })
+    //   return data.slice(start, end+1);
+    // });
 
 
-    _.each(allPoints, function(points, i) {
-      if (points.length){
-        artist.path(..._.flatten(points), {strokeStyle: colors[i]});
-      }
-    });
+
+    // var allPoints = _.map(relevantRawData, function(rawData_, i) {
+
+    //   return _.map(rawData_, function(data, i) {
+    //     // return [x + pageMargins.left + i * xInterval, y + height * ( 1 - data / maxDataValue)];
+    //     var xPos = x + i * xInterval,
+    //         yPos = y + height * ( 1 - data / maxDataValue);
+
+    //     // If we are writing a section of the sequence (not beginning) then assume that the x is positioned right
+    //     // after the previous nucleotide letter.
+    //     if (fromBase > 0){
+    //       xPos += layoutSettings.basePairDims.width/2;
+    //     }
+
+    //     return [xPos, yPos];
+    //   });
+    // });
+
+    // artist.updateStyle({
+    //   lineWidth: 1
+    // });
+
+    // _.each(allPoints, function(points, i) {
+    //   if (points.length){
+    //     artist.path(..._.flatten(points), {strokeStyle: colors[i]});
+    //   }
+    // });
 
     // artist.smoothLineGraph(points);
 
+    var normalizedWidth = layoutSettings.basePairDims.width;
+
+    function getRelevantData(data, base){
+      var start = peaks[base - 2],
+          end   = peaks[base - 1];
+
+      return data.slice(start, end+1);
+    }
+    function normalize(x, rawData){
+      var normalized = _.map(rawData, function(data, i){
+        var xIncrement = normalizedWidth/rawData.length,
+            xPos = x + (xIncrement * i),
+            yPos = y + height * ( 1 - data / maxDataValue );
+
+        return  [xPos, yPos];
+      });
+
+      return normalized;
+    }
+
+    function paint(points, color){
+      artist.path(..._.flatten(points), {
+        strokeStyle: color,
+        lineWidth: 1
+      });
+    }
+
+    function newDraw(){
+      _.forEach(['A','C','G','T'], function(nucleotide, i){
+
+        var points = [],
+            base = fromBase,
+            numBases = toBase - fromBase;
+
+        for (var j = 0; j < numBases; j++){
+          var data = getRelevantData(rawData[i], base + j),
+              normalized = normalize(x + j * normalizedWidth, data);
+
+          points = points.concat(normalized);
+        }
+
+        paint(points, colors[i]);
+      });
+    };
+
+    newDraw();
 
   }
 }
