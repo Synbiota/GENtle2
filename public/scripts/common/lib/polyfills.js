bc9ad20ea7e9585c065608f2fd37345e532973f0
Alexander James Phillips <ajp.global@gmail.com>
2015-06-10 19:33:12 +0100

    Add String.prototype.endsWith to polyfills

--- public/scripts/common/lib/polyfills.js
+++ public/scripts/common/lib/polyfills.js
@@ -190,6 +190,22 @@
     }
   }
 
+  //
+  // String.prototype.endsWith
+  // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith#Polyfill
+  //
+  if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (position === undefined || position > subjectString.length) {
+          position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
+  }
+
 
   // Polyfills mutate the global state so we export nothing
   // return;
