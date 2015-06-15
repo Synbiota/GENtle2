631a6bb0e531216ea32f1c8d80a3d680f3d2e6ee
Cliff <cliffdlau@gmail.com>
2015-06-15 16:31:42 -0400

    new chroma files.

---
+++ public/scripts/linear_map/views/linear_chromatograph_view.js
@@ -0,0 +1,323 @@
+import Backbone from 'backbone';
+import Gentle from 'gentle';
+import template from '../templates/linear_map_view.hbs';
+import _ from 'underscore';
+import RestrictionEnzymes from '../../sequence/lib/restriction_enzymes';
+
+export default Backbone.View.extend({
+  manage: true,
+  template: template,
+  className: 'linear-map',
+  minPositionMarkInterval: 60,
+  initialRender: true,
+
+  events: {
+    'click .linear-map-feature': 'goToFeature'
+  },
+
+  initialize: function() {
+    this.model = Gentle.currentSequence;
+    this.minFeatureWidth = 4;
+    this.topFeatureOffset = 0;
+    // _.bindAll(this, 'scrollSequenceCanvas');
+
+    _.bindAll(this,
+      'scrollSequenceCanvas',
+      'updateScrollHelperPosition',
+      'refresh'
+    );
+
+    this.listenTo(
+      this.model,
+      'change:sequence change:features.* change:features change:displaySettings.rows.res.*',
+      _.debounce(this.refresh, 500),
+      this
+    );
+
+    this.$el.addClass( this.horizontal ? 'horizontal' : 'vertical');
+
+  },
+
+  processFeatures: function() {
+    var id = -1,
+        _this = this;
+
+    this.features = [];
+
+    _.each(this.model.getFeatures(), function(feature) {
+      _.each(feature.ranges, function(range) {
+        _this.features.push({
+          name: feature.name,
+          id: ++id,
+          from: range.from,
+          to: range.to,
+          type: feature._type.toLowerCase()
+        });
+      });
+    });
+
+    this.features = _.sortBy(this.features, function(feature) {
+      return feature.from;
+    });
+  },
+
+  positionFeatures: function() {
+    var maxBase = this.maxBaseForCalc || this.model.getLength(),
+        viewHeight = this.$el.height(),
+        $featureElement, feature, featureWidth,
+        overlapStack = [], overlapIndex;
+
+    for(var i = 0; i < this.features.length; i++) {
+      feature = this.features[i];
+      featureWidth = Math.max(
+        Math.floor((feature.to - feature.from + 1) / maxBase * viewHeight),
+        this.minFeatureWidth
+      );
+      $featureElement = this.$('[data-feature_id="'+feature.id+'"]');
+
+      $featureElement.css({
+        width: featureWidth,
+        top: Math.floor(feature.from / maxBase * viewHeight) + this.topFeatureOffset,
+      });
+
+      overlapIndex =  overlapStack.length;
+
+      for(var j = overlapStack.length - 1; j >= 0; j--) {
+        if(overlapStack[j] === undefined || overlapStack[j][1] <= feature.from) {
+          overlapStack[j] = undefined;
+          overlapIndex = j;
+        }
+      }
+
+      $featureElement.addClass('linear-map-feature-stacked-'+overlapIndex);
+
+      overlapStack[overlapIndex] = [feature.from, feature.to];
+    }
+  },
+
+  goToFeature: function(event) {
+    var featureId = $(event.currentTarget).data('feature_id'),
+        feature = _.findWhere(this.features, {id: featureId});
+
+    this.sequenceCanvas.scrollToBase(feature.from);
+
+    event.preventDefault();
+  },
+
+  serialize: function() {
+    if(this.initialRender) {
+      return {};
+    } else {
+      return {
+        features: this.features,
+        verticalPositionMarks: this.verticalPositionMarks,
+        horizontalPositionMarks: this.horizontalPositionMarks,
+        enzymes: this.enzymes
+      };
+    }
+  },
+
+  displayEnzymes: function() {
+    return this.model.get('displaySettings.rows.res.display');
+  },
+
+  refresh: function(render) {
+    this.processFeatures();
+    this.processPositionMarks();
+    if(this.displayEnzymes()) {
+      this.processEnzymes();
+    } else {
+      this.enzymes = [];
+    }
+
+    if(render !== false) this.render();
+  },
+
+  processPositionMarks: function() {
+    var sequenceCanvas = this.sequenceCanvas,
+        length = this.horizontal ? this.$el.width() : this.$el.height(),
+        maxBase = sequenceCanvas.maxVisibleBase(),
+        magnitudeOrder = Math.floor(Math.log10(maxBase)),
+        divider = Math.pow(10, magnitudeOrder - 1),
+        maxBaseForCalc = Math.ceil(maxBase / divider) * divider,
+        positionMarks = [];
+
+    this.positionMarksInterval = Math.floor(maxBaseForCalc / 10);
+    this.maxBaseForCalc = maxBaseForCalc;
+
+    while(this.positionMarksInterval / this.maxBase * length < this.minPositionMarkInterval) {
+      this.positionMarksInterval = Math.floor(this.positionMarksInterval * 2);
+    }
+
+    for(var i = 0; i < maxBase; i += this.positionMarksInterval) {
+
+      var data = {
+            base: i,
+            label: _.formatThousands(i+1),
+            offset: Math.floor(i / maxBase * length)
+          };
+
+      positionMarks.push(data);
+    }
+
+    if (this.horizontal){
+      this.horizontalPositionMarks = positionMarks;
+    } else {
+      this.verticalPositionMarks = positionMarks;
+    }
+
+
+  },
+
+  processEnzymes: function() {
+    var model = this.model;
+    var displaySettings = model.get('displaySettings.rows.res') || {};
+    var enzymes = RestrictionEnzymes.getAllInSeq(model.getSequence(), {
+      // length: displaySettings.lengths || [],
+      customList: displaySettings.custom || [],
+      // hideNonPalindromicStickyEndSites: displaySettings.hideNonPalindromicStickyEndSites || false
+      hideNonPalindromicStickyEndSites: false
+    });
+
+    this.enzymes = _.compact(_.map(enzymes, function(enzymeArray, position) {
+      position = position^0;
+
+      enzymeArray = _.filter(enzymeArray, function(enzyme) {
+        return model.isRangeEditable(position, position + enzyme.seq.length);
+      });
+
+      if(enzymeArray.length === 0) return;
+      var label = enzymeArray[0].name;
+      if(enzymeArray.length > 1) label += ' +' + (enzymeArray.length - 1);
+      return {position, label};
+    }));
+  },
+
+  positionEnzymes: function() {
+    var maxBase = this.sequenceCanvas.maxVisibleBase();
+
+    _.each(this.$('.linear-map-enzyme'), (element) => {
+      var $element = this.$(element);
+      var relativePosition = $element.data('position')/ maxBase * 100 ;
+      $element.css('top', relativePosition + '%');
+    });
+  },
+
+  setupScrollHelper: function() {
+    var sequenceCanvas = this.sequenceCanvas,
+        $scrollHelper = this.$('.linear-map-visible-range'),
+        scrollingParentHeight = sequenceCanvas.$scrollingParent.height(),
+        scrollingChildHeight = sequenceCanvas.$scrollingChild.height(),
+        elemHeight = this.$el.height(),
+        elemWidth = this.$el.width();
+
+
+    this.$scrollHelper = $scrollHelper;
+
+    if (this.horizontal){
+      $scrollHelper.width(Math.floor(
+        scrollingParentHeight /
+        scrollingChildHeight *
+        elemWidth
+      ));
+    } else {
+      $scrollHelper.height(Math.floor(
+        scrollingParentHeight /
+        scrollingChildHeight *
+        elemHeight
+      ));
+    }
+
+    $scrollHelper.draggable({
+        axis: 'y',
+        containment: 'parent',
+        drag: _.throttle(this.scrollSequenceCanvas, 50)
+      });
+
+    this.updateScrollHelperPosition();
+  },
+
+  updateScrollHelperPosition: function() {
+    var sequenceCanvas = this.sequenceCanvas,
+        $scrollHelper = this.$scrollHelper,
+        scrollingChildHeight = sequenceCanvas.$scrollingChild.height(),
+        elemHeight = this.$el.height(),
+        elemWidth = this.$el.width();
+
+    if($scrollHelper) {
+      if (this.horizontal){
+        $scrollHelper.css({
+          left:  Math.floor( sequenceCanvas.layoutHelpers.yOffset /
+                            scrollingChildHeight *
+                            elemWidth)
+        });
+      } else {
+        $scrollHelper.css({
+          top:  Math.floor( sequenceCanvas.layoutHelpers.yOffset /
+                            scrollingChildHeight *
+                            elemHeight)
+        });
+      }
+
+    }
+  },
+
+  scrollSequenceCanvas: function() {
+    this.sequenceCanvas.scrollTo(Math.floor(
+      this.$scrollHelper.position().top /
+      this.$el.height() *
+      this.sequenceCanvas.$scrollingChild.height()
+    ), false);
+  },
+
+  afterRender: function() {
+    if(this.initialRender) {
+      var sequenceCanvas = this.parentView().sequenceCanvas;
+      this.initialRender = false;
+      this.sequenceCanvas = sequenceCanvas;
+
+      // this.listenTo(
+      //   sequenceCanvas,
+      //   'scroll',
+      //   this.updateScrollHelperPosition,
+      //   this
+      // );
+
+      // this.listenTo(
+      //   sequenceCanvas,
+      //   'change:layoutHelpers',
+      //   this.refresh,
+      //   this
+      // );
+
+      sequenceCanvas.on(
+        'scroll',
+        this.updateScrollHelperPosition
+      );
+
+      sequenceCanvas.on(
+        'change:layoutHelpers',
+        this.refresh
+      );
+
+    } else {
+
+      this.setupScrollHelper();
+      this.positionFeatures();
+      if(this.displayEnzymes()) this.positionEnzymes();
+
+      // When SequenceCanvas' layoutHelpers are calculated, we fetch the
+      // max base number (>= sequence length)
+      // sequenceCanvas.redraw();
+      // sequenceCanvas.afterNextRedraw(function() {
+      //   var layoutHelpers = sequenceCanvas.layoutHelpers;
+      //   _this.maxBase = layoutHelpers.rows.total * layoutHelpers.basesPerRow - 1;
+
+      //   _this.setupScrollHelper();
+
+      //   _this.positionFeatures();
+      // });
+    }
+  },
+
+});
---
+++ public/scripts/sequence/views/sequence_chromatograph_view.js
@@ -0,0 +1,125 @@
+import template from '../templates/sequence_edition_view.hbs';
+import SequenceCanvas from '../lib/sequence_canvas';
+import Gentle from 'gentle';
+import ContextMenuView from '../../common/views/context_menu_view';
+import LinearMapView from '../../linear_map/views/linear_map_view';
+import PlasmidMapView from '../../plasmid_map/views/plasmid_map_view';
+import MatchedEnzymesView from './matched_enzymes_view';
+import Backbone from 'backbone';
+import Q from 'q';
+
+/**
+@module Sequence
+@submodule Views
+@class SequenceEditionView
+**/
+export default Backbone.View.extend({
+  manage: true,
+  template: template,
+  className: 'sequence-view',
+
+  initialize: function() {
+    var _this = this;
+
+    this.model = Gentle.currentSequence;
+
+    this.contextMenuView = new ContextMenuView({context: 'sequence'});
+
+    this.matchedEnzymesView = new MatchedEnzymesView();
+    this.setView('.sequence-matched-enzymes-outlet', this.matchedEnzymesView);
+
+    this.initSecondaryViews();
+  },
+
+  initSecondaryViews: function(trigger) {
+    var secondaryViews,
+        currentView;
+
+    secondaryViews = _.chain(Gentle.plugins)
+      .where({type: 'sequence-secondary-view'})
+      .pluck('data')
+      .value();
+
+    secondaryViews.push({
+      name: 'linear',
+      title: 'Linear map',
+      view: LinearMapView
+    });
+
+    secondaryViews.push({
+      name: 'plasmid',
+      title: 'Plasmid map',
+      view: PlasmidMapView
+    });
+
+    currentView = this.model.get('isCircular') ? 'plasmid' : 'linear';
+
+    // if(!~_.pluck(secondaryViews, 'name').indexOf(currentView))
+    //   currentView = 'linear';
+
+    this.secondaryViews = secondaryViews;
+    this.changeSecondaryView(currentView, false);
+  },
+
+  handleResizeRight: function(trigger) {
+    $('#sequence-canvas-primary-view-outlet, .sequence-canvas-outlet').css({
+      'right': this.secondaryView.$el.width(),
+    });
+    if(trigger !== false) {
+      this.trigger('resize');
+      this.secondaryView.trigger('resize');
+    }
+  },
+
+  secondaryViewRightPos: function() {
+    return $('#sequence-secondary-view-outlet').width();
+  },
+
+   primaryViewLeftPos: function() {
+    return $('#sequence-primary-view-outlet').width();
+  },
+
+  changeSecondaryView: function(viewName, render) {
+    var secondaryViewClass = _.findWhere(this.secondaryViews, {name: viewName});
+    if(this.secondaryView) this.secondaryView.remove();
+    this.secondaryView = new secondaryViewClass.view();
+
+    this.model.set('displaySettings.secondaryView', viewName).throttledSave();
+
+    this.setView('#sequence-canvas-secondary-view-outlet', this.secondaryView);
+    var secondaryViewPromise;
+    if(render !== false) {
+      secondaryViewPromise = this.secondaryView.render();
+    } else {
+      secondaryViewPromise = Q.resolve();
+    }
+    // `handleResizeRight` requires secondaryView to be rendered so that
+    // it (`this.secondaryView.$el.width()`) is the correct value.
+    secondaryViewPromise.then(() => {
+      this.handleResizeRight(false);
+      if(render !== false) {
+       this.sequenceCanvas.refresh();
+      }
+    });
+  },
+
+  afterRender: function() {
+    this.$('.sequence-canvas-outlet').css({
+      'right': this.secondaryView.$el.width(),
+    });
+
+    var sequence = this.model;
+
+    var sequenceCanvas = this.sequenceCanvas = new SequenceCanvas({
+      sequence: sequence,
+      container: this.$('.sequence-canvas-outlet').first(),
+      yOffset: sequence.get('displaySettings.yOffset'),
+      // non library options
+      contextMenu: this.contextMenuView,
+      view: this
+    });
+
+    sequenceCanvas.refresh();
+  },
+
+});
