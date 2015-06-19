b1d4f2676ab25a506bd84b66ae218eabb13aaaec
Alexander James Phillips <ajp.global@gmail.com>
2015-06-19 16:07:30 +0100

    WipPcrProductSequence is a temporary sequence

--- public/scripts/plugins/pcr/lib/wip_product.js
+++ public/scripts/plugins/pcr/lib/wip_product.js
@@ -1,29 +1,16 @@
-import Sequence from '../../../sequence/models/sequence';
-import _ from 'underscore';
+import TemporarySequence from '../../../sequence/models/temporary_sequence';
 
 
-class WipPcrProductSequence extends Sequence {
+class WipPcrProductSequence extends TemporarySequence {
   constructor() {
     super(...arguments);
     this.set('_type', 'wip_pcr_product', {silent: true});
-  }
-
-  get requiredFields() {
-    return super.requiredFields.concat([
-      // 'forwardPrimer',
-      // 'reversePrimer',
-      // 'stickyEnds'
-    ]);
   }
 
   get optionalFields() {
     return super.optionalFields.concat([
       'inProgress',
       'sourceSequenceName',
-      // the following might be unnecessary
-      'forwardPrimer',
-      'reversePrimer',
-      'stickyEnds'
     ]);
   }
 }
