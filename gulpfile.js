250e2358c61767e9e35045e262fc0eca7add5c76
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-06-22 18:51:25 +0100

    Deploying to AWS OpsWorks using node AWS SDK

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 
 require('./tasks/javascript');
 require('./tasks/css');
+require('./tasks/deploy');
 
 gulp.task('build', ['js', 'css']);
 gulp.task('default', ['js:watch', 'css:watch']);
---
+++ tasks/deploy.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var AWS = require('aws-sdk');
+var opsworks = new AWS.OpsWorks();
+
+
+//"aws --region='us-east-1' opsworks create-deployment --stack-id $AWS_NIGHTLY_STACK_ID --app-id $AWS_NIGHTLY_APP_ID --command '{\"Name\": \"deploy\", \"Args\": {\"migrate\": [\"false\"]}}'"
+
+gulp.task('deploy', function() {
+
+  var STACK_ID = process.env.STACK_ID;
+  if(!STACK_ID) throw 'STACK_ID environment variable missing';
+
+  var APP_ID = process.env.APP_ID;
+  if(!APP_ID) throw 'APP environment variable missing';
+
+  var params = {
+    StackId: STACK_ID,
+    AppId: APP_ID,
+    Command: {
+      Name: 'deploy',
+      Args: {
+        migrate: ['false']
+      }
+    }
+  };
+
+  opsworks.createDeployment(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+});
