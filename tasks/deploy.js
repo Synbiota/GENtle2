a3e9b9fff8b90e24d21a64e231a4a69c96c7c9fc
Alexandre Meunier <alexandre.meunier@gmail.com>
2015-06-22 19:20:56 +0100

    Updating App ENV VARS before deploying

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -5,7 +5,7 @@ var opsworks = new AWS.OpsWorks({region: 'us-east-1'});
 
 //"aws --region='us-east-1' opsworks create-deployment --stack-id $AWS_NIGHTLY_STACK_ID --app-id $AWS_NIGHTLY_APP_ID --command '{\"Name\": \"deploy\", \"Args\": {\"migrate\": [\"false\"]}}'"
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['publish'], function() {
 
   var STACK_ID = process.env.STACK_ID;
   if(!STACK_ID) throw 'STACK_ID environment variable missing';
@@ -13,7 +13,21 @@ gulp.task('deploy', function() {
   var APP_ID = process.env.APP_ID;
   if(!APP_ID) throw 'APP environment variable missing';
 
-  var params = {
+  var manifest = require('../rev-manifest');
+  var customJson = {
+    revManifest: manifest
+  };
+
+  var updateParams = {
+    AppId: APP_ID,
+    Environment: [{
+      Key: 'REV_MANIFEST', 
+      Value: JSON.stringify(customJson), 
+      Secure: false
+    }]
+  };
+
+  var deployParams = {
     StackId: STACK_ID,
     AppId: APP_ID,
     Command: {
@@ -21,11 +35,22 @@ gulp.task('deploy', function() {
       Args: {
         migrate: ['false']
       }
-    }
+    },
+    CustomJson: JSON.stringify(customJson)
   };
 
-  opsworks.createDeployment(params, function(err, data) {
+  opsworks.updateApp(updateParams, function(err) {
     if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
+    else {
+      opsworks.createDeployment(deployParams, function(err_) {
+        if (err_) console.log(err_, err_.stack); // an error occurred
+      });
+    }
   });
+  
+});
+
+
+gulp.task('publish', ['build'], function() {
+  // TODO publish to s3
 });
