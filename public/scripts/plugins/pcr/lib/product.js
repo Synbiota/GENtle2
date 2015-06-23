d0a96d8e79ff1487c7435fdf73777ff3370872bd
Alexander James Phillips <ajp.global@gmail.com>
2015-06-23 13:44:10 +0100

    Force PcrProductSequence to have pcr_product _type and be readonly

--- public/scripts/plugins/pcr/lib/product.js
+++ public/scripts/plugins/pcr/lib/product.js
@@ -2,6 +2,12 @@ import Sequence from '../../../sequence/models/sequence';
 
 
 class PcrProductSequence extends Sequence {
+  constructor(attrs, ...args) {
+    attrs._type = 'pcr_product';
+    attrs.readOnly = true;
+    super(attrs, ...args);
+  }
+
   get requiredFields() {
     return super.requiredFields.concat([
       'forwardPrimer',
