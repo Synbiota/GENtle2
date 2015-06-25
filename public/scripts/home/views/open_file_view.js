e1ef7a66d3f06a530032c80d084d72f47cd5a2ee
Alexander James Phillips <ajp.global@gmail.com>
2015-06-25 14:39:58 +0100

    Home view errors on failure to parse and instantiate sequence

--- public/scripts/home/views/open_file_view.js
+++ public/scripts/home/views/open_file_view.js
@@ -3,14 +3,14 @@
 @submodule Views
 @class OpenFileView
 **/
-// define(function(require) {
-  var Backbone    = require('backbone'),
-      template    = require('../templates/open_file_view.hbs'),
-      Filetypes   = require('../../common/lib/filetypes/filetypes'),
-      Gentle      = require('gentle'),
-      OpenFileView;
+import _ from 'underscore';
+import Backbone from 'backbone';
+import template from '../templates/open_file_view.hbs';
+import Filetypes from '../../common/lib/filetypes/filetypes';
+import Gentle from 'gentle';
 
-  OpenFileView = Backbone.View.extend({
+
+  var OpenFileView = Backbone.View.extend({
     manage: true,
     template: template,
     className: 'home-open-file',
@@ -23,17 +23,23 @@
     openSequenceFromFile: function(event) {
       event.preventDefault();
       var $form     = $('.home-open-file-form').first(),
-          input     = $form.find('input[name=file]')[0],
-          _this     = this;
+          input     = $form.find('input[name=file]')[0];
 
       var onLoad = function(result) {
-        Filetypes.guessTypeAndParseFromArrayBuffer(result.content, result.name).then ( function ( sequences ) {
-          if ( sequences.length ) Gentle.addSequencesAndNavigate(sequences);
-           else alert('Could not parse the sequence.');
-        } , function (err) {
-          console.log(err);
+        Filetypes.guessTypeAndParseFromArrayBuffer(result.content, result.name)
+        .then(function(sequences) {
+          if(sequences.length) {
+            Gentle.addSequencesAndNavigate(sequences);
+          } else {
+            console.error('Filename: ', result.name, '\nsequences: ', sequences);
+            alert('Could not parse the sequence.');
+          }
+        })
+        .catch(function(err) {
+          console.error(err, '\nFilename: ', result.name);
           alert('Could not parse the sequence.');
-        } ) ;
+        })
+        .done();
       };
 
       var onError = function(filename) {
@@ -52,5 +58,3 @@
 
   });
 export default OpenFileView;
-  // return OpenFileView;
-// });
