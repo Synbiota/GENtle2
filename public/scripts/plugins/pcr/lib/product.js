c40b3197a9d1a3a4a81c0742e6d47372ab20d310
Alexander James Phillips <ajp.global@gmail.com>
2015-07-16 12:52:16 +0100

    Add simple validation for presence of stickyEnds attribute value

--- public/scripts/plugins/pcr/lib/product.js
+++ public/scripts/plugins/pcr/lib/product.js
@@ -17,6 +17,14 @@ class PcrProductSequence extends Sequence {
       'stickyEnds'
     ]);
   }
+
+  validateFields(attributes) {
+    super.validateFields(attributes);
+    // Remove once stickyEnds are a class in their own right and their
+    // constructor would raise this error instead / return an empty stickyEnds
+    // instance
+    if(!attributes.stickyEnds) throw new TypeError(`stickyEnds provided as attribute but were: ${attributes.stickyEnds}`);
+  }
 }
 
 
