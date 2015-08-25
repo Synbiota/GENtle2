74ef78414085d2070c7298891cfd60c18119d7e4
Alexander James Phillips <ajp.global@gmail.com>
2015-08-25 18:31:57 +0100

    Polish, use the same rdp_pcr_product constant string value

--- public/scripts/library/rdp/rdp_pcr_sequence.js
+++ public/scripts/library/rdp/rdp_pcr_sequence.js
@@ -4,9 +4,10 @@ import RdpEdit from './rdp_edit';
 import SequencesCollection from '../../sequence/models/sequences';
 
 
+var rdpPcrProduct = 'rdp_pcr_product';
+
 class RdpPcrSequence extends PcrProductSequence {
   constructor(attrs, ...args) {
-    var rdpPcrProduct = 'rdp_pcr_product';
     if(attrs._type && attrs._type !== rdpPcrProduct) {
       throw new TypeError(`RdpPcrSequence expected _type of "${rdpPcrProduct}" but was "${attrs._type}"`);
     }
@@ -35,6 +36,6 @@ var version1RdpPcrSequencePreProcessor = version1GenericPreProcessor('rdpEdits')
 RdpPcrSequence.registerPreProcessor(version1RdpPcrSequencePreProcessor);
 RdpPcrSequence.registerAssociation(RdpEdit, 'rdpEdit', true);
 
-SequencesCollection.registerConstructor(RdpPcrSequence, 'rdp_pcr_product');
+SequencesCollection.registerConstructor(RdpPcrSequence, rdpPcrProduct);
 
 export default RdpPcrSequence;
