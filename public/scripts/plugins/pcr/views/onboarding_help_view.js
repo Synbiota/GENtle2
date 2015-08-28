2b2e60c3cfd21f9a0c0457f1c40413b5f862c020
Dave Russell <dave.kerr@gmail.com>
2015-08-28 15:58:09 +0100

    pre-demo

--- public/scripts/plugins/pcr/views/onboarding_help_view.js
+++ public/scripts/plugins/pcr/views/onboarding_help_view.js
@@ -1,6 +1,7 @@
 import {View} from 'backbone';
 import template from '../templates/onboarding_help_view.hbs';
 import Gentle from 'gentle';
+import Modal from '../../../common/views/modal_view';
 
 const hideModalKey = 'newRdpPartHideModal';
 
@@ -9,7 +10,8 @@ export default View.extend({
   template: template,
 
   events: {
-    'change input': 'toggleShowNextTime'
+    'change input': 'toggleShowNextTime',
+    'click  #choose-type': 'dismiss'
   },
 
   initialize: function(options) {
@@ -23,5 +25,12 @@ export default View.extend({
   toggleShowNextTime(event) {
     var checked = this.$(event.target).is(':checked');
     Gentle.currentUser.set(hideModalKey, !checked);
+  },
+
+  dismiss: function() {
+    console.log(this.parentView());
+    //console.log(this.parentView.getView());
+    //this.parentView().hide();
+    //Modal.hide();
   }
 });
