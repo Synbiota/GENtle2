51a9ca3ca535e9b75f6a1f8cddecb4edf7065600
Cliff <cliffdlau@gmail.com>
2015-10-05 19:35:03 -0400

    Now routes to home if preloaded sequences are added.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -63,6 +63,9 @@ var addableSequences = _.filter(preloadedSequences, function(sequence) {
 
 if(addableSequences.length) {
   Gentle.addSequences(addableSequences);
+  _.defer(function(){
+    Gentle.router.home()
+  })
 }
 
 // end custom build
